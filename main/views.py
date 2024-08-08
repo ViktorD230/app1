d6a44b773704b14e5e2c81fdff14e8bf6e0275ce
VikD230 <Vikd230@gmail.com>
2024-08-08 13:44:40 +0300

    The main app is ready, pages index and about

--- main/views.py
+++ main/views.py
@@ -1,15 +1,20 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     context = {
-        'title': "Home",
-        'content': 'Главная страница магазина',
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME',
     }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('About Page')
+     context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Магазин мебели HOME предлагает высококачественную мебель, которая сочетает в себе стиль, комфорт и долговечность. В нашем ассортименте вы найдете широкий выбор товаров для дома: удобные диваны и кресла, функциональные столы и стулья, просторные шкафы и комоды. Каждый предмет мебели выполнен с вниманием к деталям и из качественных материалов, что гарантирует их долговечность и прочность. Мы стремимся предложить нашим клиентам лучшие решения для создания уютного и стильного интерьера, который будет радовать вас долгие годы.'
+
+    }
+     return render(request, 'main/about.html', context)
