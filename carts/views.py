731fc6c390d205c96df9d7e77e22c656296a9a08
VikD230 <Vikd230@gmail.com>
2024-09-13 11:37:55 +0300

    Sessions - carts for anonymous user

--- carts/views.py
+++ carts/views.py
@@ -6,6 +6,7 @@ from carts.models import Cart
 from carts.utils import get_user_carts
 from goods.models import Products
 
+
 def cart_add(request):
 
     product_id = request.POST.get("product_id")
@@ -24,6 +25,19 @@ def cart_add(request):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
+    else:
+        carts = Cart.objects.filter(
+            session_key=request.session.session_key, product=product)
+        
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+
+        else:
+            Cart.objects.create(
+                session_key=request.session.session_key, product=product, quantity=1)
 
     user_cart = get_user_carts(request)
     cart_items_html = render_to_string(
@@ -35,6 +49,7 @@ def cart_add(request):
     }
 
     return JsonResponse(responce_data)
+
 
 
 def cart_change(request):
